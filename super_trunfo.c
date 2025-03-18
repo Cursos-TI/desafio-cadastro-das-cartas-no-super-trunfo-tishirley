eb559d7ee0b48b254e269add0f5ff5a0aef9bbfe
tishirley <vipconvitesjf@gmail.com>
2025-03-18 18:22:20 +0000

    Nomeei o arquivo

---
+++ super_trunfo.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(){
+    char estado;
+    char codigo[3];
+    char nome[20];
+    int populacao;
+    float area;
+    float pib;
+    int pontos;
+
+
+    printf("Desafio Novato: Cadastro das Cartas\n");
+    printf("\n");
+
+    printf("Carta 1:\n");
+    printf("Estado: A\n");
+    printf("Código: A01\n");
+    printf("Nome da Cidade: São Paulo\n");
+    printf("População: 12325000\n");
+    printf("Área: 1521.11 km²\n");
+    printf("PIB: 699.28 bilhões de reais\n");
+    printf("Números de Pontos Turísticos: 50\n");
+    printf("\n");
+
+
+    printf("Carta 2:\n");
+    printf("Estado: B\n");
+    printf("Código: B01\n");
+    printf("Nome da Cidade: Rio de Janeiro\n");
+    printf("População: 6748000\n");
+    printf("Área: 1200.25 km²\n");
+    printf("PIB: 300.50 bilhões de reais\n");
+    printf("Número de Pontos Turísticos: 30\n");
+    printf("\n");
+
+    return 0;
+
+}
