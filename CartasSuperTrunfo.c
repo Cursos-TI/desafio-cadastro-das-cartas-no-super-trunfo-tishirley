1c4a6b19cd2af8f71f4b1d88420f8e74ca306500
tishirley <vipconvitesjf@gmail.com>
2025-03-18 17:30:16 +0000

    Criei um cadastro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
 int main(){
+    char estado;
+    char codigo[3];
+    char nome[20];
+    int populacao;
+    float area;
+    float pib;
+    int pontos;
+
+
     printf("Desafio Novato: Cadastro das Cartas\n");
+
+    printf("Carta 1:\n");
+    printf("Estado:\n");
+    printf("Código:\n");
+    printf("Nome da Cidade:\n");
+    printf("População:\n");
+    printf("Área:\n");
+    printf("PIB:\n");
+    printf("Números de Pontos Turísticos:\n");
 
 }
