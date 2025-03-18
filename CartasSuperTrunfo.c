0683e36f21871d4db3d724f3a73a7d4670935e4d
tishirley <vipconvitesjf@gmail.com>
2025-03-18 17:43:24 +0000

    Identificando variaveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,10 +4,10 @@ int main(){
     char estado;
     char codigo[3];
     char nome[20];
-    int populacao;
-    float area;
-    float pib;
-    int pontos;
+    int populacao = 12325000;
+    float area = 1521.11;
+    float pib = 699.28;
+    int pontos = 50;
 
 
     printf("Desafio Novato: Cadastro das Cartas\n");
